e0d1953d2be750ce5dfdabf4ebdcadf29c95efbb
heru.ghovari <heru.ghovari@bankraya.co.id>
2024-08-04 00:31:35 +0700

    fix: revisi path API send code verification email

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -15,7 +15,7 @@ const router = Router();
 
 router.post("/register", validationRegistration, signUp);
 router.post("/login", validationLogin, signIn, sendVerificationCode);
-router.post("/sendVerivicationCode", sendVerificationCode);
+router.post("/sendVerificationCode", sendVerificationCode);
 router.post("/emailVerification", emailVerification);
 
 export default router;
