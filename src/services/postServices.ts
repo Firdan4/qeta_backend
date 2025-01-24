b565b0e383b17944ef39f7b4585e208486b290d6
amriyasirli <amriyasirli62@gmail.com>
2025-01-25 06:09:02 +0700

    refactor(post): join to user field

--- src/services/postServices.ts
+++ src/services/postServices.ts
@@ -1,19 +1,33 @@
 import { Op, Sequelize } from "sequelize";
 import Post from "../db/models/post";
 import Like from "../db/models/like";
+import User from "../db/models/user";
 
 export const getRandomPost = async (
   userId?: number
 ): Promise<Post[] | null> => {
   const post = await Post.findAll({
     order: [Sequelize.fn("RAND")],
-    limit: 10,
+    // limit: 10,
     where: {
       idUser: {
         [Op.ne]: userId, // Kondisi: idUser tidak sama dengan user saat ini
       },
     },
-    include: [{ model: Like, as: "like" }],
+    include: [
+      { model: Like, as: "like" },
+      {
+        model: User,
+        as: "user",
+        attributes: [
+          "firstName",
+          "lastName",
+          "displayName",
+          "photoURL",
+          "verifiedAccount",
+        ],
+      },
+    ],
     // group: ["idUser"], // Disabled Sementara
   });
 
